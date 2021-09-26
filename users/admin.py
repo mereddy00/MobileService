cf0662e2ffff42d201f6543bb83d759f3e24a0fd
mereddy00 <78505794+mereddy00@users.noreply.github.com>
2021-09-26 18:15:48 -0500

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
-from users.forms import CustomUserCreationForm, CustomUserChangeForm
 from users.models import CustomUser
 
 
